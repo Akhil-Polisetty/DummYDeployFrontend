642d67ad29614ac83b51b46d9ec2e2ed6808eb0e
Akhil-Polisetty <polisettiakhi72073@gmail.com>
2025-05-11 15:30:33 +0530

    second commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // adjust as per your backend
+  baseURL: "https://duumy-deploy1.vercel.app/api", // adjust as per your backend
   withCredentials: true, // if using cookies/sessions
 });
 
